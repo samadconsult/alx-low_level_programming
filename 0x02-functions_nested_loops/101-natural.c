8353f1c39bba2899e1f2ce2f5e4f1c91f1d35c7d
samadconsult <mamidumoshood1990@gmail.com>
2022-12-14 13:15:56 +0100

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- *  * main - Gets the sum of mutliples of 3 or 5 until 1024.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - Gets the sum of mutliples of 3 or 5 until 1024.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
-		int i;
-			int sum = 0;
+	int i;
+	int sum = 0;
 
-				for (i = 0; i < 1024; i++)
-						{
-									if ((i % 3) == 0  || (i % 5) == 0)
-													sum += i;
-										}
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0  || (i % 5) == 0)
+			sum += i;
+	}
 
-					printf("%d\n", sum);
+	printf("%d\n", sum);
 
-						return (0);
+	return (0);
 }
 
